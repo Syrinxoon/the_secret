0cdbc3cd0eef78050f1eaad3d40e741044400ef9
Nathan Le Ray <oyokoon@syrinxoon.net>
2013-02-03 18:29:18 +0100

    updated routes for Rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ TheSecret::Application.routes.draw do
   root :to => 'home#index'
   
   get "secret", to: "home#secret", as: :secret
-  get 'auth/:provider/callback', to: 'authentications#omniauth'
-  get 'auth/failure', to: redirect('/')
+  match 'auth/:provider/callback', to: 'authentications#omniauth', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
   resource :session
   resource :profile
   resources :users
